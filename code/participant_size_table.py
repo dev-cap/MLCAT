d4cb6390347183057c85d3704610cb5f0f7b9183
Achyudh Ram <achyudhk@gmail.com>
2016-02-04 21:17:46 +0530

    Refactor Code, Add Participation-SIze Table and Author Graph Generation
        - Various modules have now been refactored as callable functions
        - A new module has been added to generate participation-size table. The data/graphs/csv folder contains the required table in CSV format. Addresses Issue #17.
       - A new module has been added that creates a Networkx graph object with the authors graph. This objet can be saved in various formats and a dendogram is generated. Addresses Issue #13 and #10.

---
+++ code/participant_size_table.py
@@ -0,0 +1,55 @@
+import networkx as nx
+import csv
+
+
+def generate_participant_size_table():
+    # TODO: Generate the table for a given time interval passed as a parameter.
+    discussion_graph = nx.DiGraph()
+
+    # Add nodes into NetworkX graph by reading CSV files
+    with open("graph_nodes.csv", "r") as node_file:
+        for pair in node_file:
+            node = pair.split(';', 2)
+            discussion_graph.add_node(node[0], time=node[2].strip(), color="#ffffff", style='bold', sender=node[1].strip())
+        node_file.close()
+    print("Nodes added.")
+
+    # Add edges into NetworkX graph by reading CSV files
+    with open("graph_edges.csv", "r") as edge_file:
+        for pair in edge_file:
+            edge = pair.split(';')
+            edge[1] = edge[1].strip()
+            try:
+                discussion_graph.node[edge[0]]['sender']
+                discussion_graph.node[edge[1]]['sender']
+                discussion_graph.add_edge(*edge)
+            except KeyError:
+                pass
+        edge_file.close()
+    print("Edges added.")
+
+    max_nodes = max(nx.number_of_nodes(i) for i in nx.weakly_connected_component_subgraphs(discussion_graph))
+    print("Maximum number of mails in a thread:", max_nodes)
+
+    # This list stores the number of authors in all threads of a given size. If no_of_authors[i] = k, there are a total
+    # of k authors in all threads of size i.
+    no_of_authors = [0 for x in range(max_nodes+1)]
+
+    for conn_subgraph in nx.weakly_connected_component_subgraphs(discussion_graph):
+        authors = set()
+        for node, attributes in conn_subgraph.nodes_iter(data=True):
+            authors.add(attributes['sender'])
+        no_of_authors[nx.number_of_nodes(conn_subgraph)] += len(authors)
+
+    # print(no_of_authors)
+    with open('participant_size_table.csv', 'w') as csvfile:
+        tablewriter = csv.writer(csvfile)
+        nmails = 0
+        tablewriter.writerow(["No. of mails", "No. of authors"])
+        for row in no_of_authors:
+            if row != 0:
+                tablewriter.writerow([nmails] + [row])
+            nmails += 1
+        csvfile.close()
+
+generate_participant_size_table()
