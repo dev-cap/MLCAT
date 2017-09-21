6cf794e9d6457d5984c272d4f47477051e62cbcf
Yash Pungaliya <yashpungaliya@gmail.com>
2017-09-21 09:52:22 +0530

    Update Input Docs

--- lib/input/mbox/keyword_digest.py
+++ lib/input/mbox/keyword_digest.py
@@ -13,15 +13,15 @@ from util.read import *
 
 def get_top_authors(top_n, json_filename):
     """
-    Gets top n authors based on the ranking generated from generate_author_ranking in analysis.author.ranking
+    Gets the top n authors based on the ranking generated from generate_author_ranking in analysis.author.ranking
 
-    :param top_n: Number of top authors to be returned.
+    :param top_n: The number of top authors to be returned.
     :param json_filename: The JSON file from which author scores are generated.
     :return: Top authors and their indices
     """
     top_authors = set()
     top_authors_index = dict()
-    author_scores = ranking.generate_author_ranking(json_filename, output_filename=None, active_score=2, passive_score=1, write_to_file=False)
+    author_scores = generate_author_ranking(json_filename, output_filename=None, active_score=2, passive_score=1, write_to_file=False)
     index = 0
     for email_addr, author_score in author_scores:
         index += 1
@@ -34,7 +34,10 @@ def get_top_authors(top_n, json_filename):
 
 def save_sparse_csr(filename, array):
     """
-    This function writes a numpy matrix to a file given as a parameter, in a sparse format.
+    This function writes a numpy matrix to a file,given as a parameter, in a sparse format.
+
+    :param filename: The file to store the matrix.
+    :param array: The numpy array.
     """
     np.savez(filename,data = array.data ,indices=array.indices,
              indptr =array.indptr, shape=array.shape )
@@ -42,10 +45,10 @@ def save_sparse_csr(filename, array):
 
 def get_message_body(message):
     """
-    Gets the message body from the message passed as the parameter.
+    Gets the message body of the message passed as a parameter.
 
     :param message: The message whose body is to be extracted.
-    :return: Message Body extracted from the message.
+    :return: The message body from the message.
     """
     msg_body = None
     if message.is_multipart():
@@ -93,7 +96,6 @@ def get_message_body(message):
 
 def generate_keyword_digest(mbox_filename, output_filename, author_uid_filename, json_filename, top_n = None, console_output=True):
     """
-
     From the .MBOX file, this function extracts the email content is extracted using two predefined classes
     available in the Python Standard Library: Mailbox and Message. Feature vectors are created for all the authors
     by obtaining meaningful words from the mail content, after removing the stop words, using NLTK libraries.
@@ -106,7 +108,6 @@ def generate_keyword_digest(mbox_filename, output_filename, author_uid_filename,
 
     :param mbox_filename: Contains the absolute or relative address of the MBOX file to be opened
     :return: Term Document Matrix: The columns of the matrix are the users and the rows of the matrix are the keywords.
-
     """
     english_stopwords = set(stopwords.words('english')) | custom_stopwords.common_words | custom_stopwords.custom_words
     email_re = re.compile(r'[\w\.-]+@[\w\.-]+')
@@ -230,4 +231,4 @@ def generate_keyword_digest(mbox_filename, output_filename, author_uid_filename,
 
         return top_authors_index, term_document_matrix, feature_names
 
-# generate_keyword_digest("lkml.mbox")
+    # generate_keyword_digest("lkml.mbox")
