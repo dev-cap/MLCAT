10fbbc04e90fa05af1b65998c398a505d268fca1
2015A7PS052G <f2015052@goa.bits-pilani.ac.in>
2017-11-16 00:33:20 +0530

    Uncomment vertex clustering

--- driver_author_analysis.py
+++ driver_author_analysis.py
@@ -5,7 +5,7 @@ from lib.analysis.author.wh_table import generate_wh_table_authors
 from lib.analysis.thread.hypergraph import generate_hyperedge_distribution
 from lib.input.mbox.keyword_clustering import generate_kmeans_clustering
 from lib.input.mbox.keyword_digest import generate_keyword_digest
-# from lib.analysis.author.community import vertex_clustering
+from lib.analysis.author.community import vertex_clustering
 
 # mailbox_list = ['lkml', 'opensuse-kernel', 'opensuse-features', 'opensuse', 'opensuse-bugs', 'opensuse-factory', 'sakai-devel']
 mailbox_list = ['opensuse-kernel']
@@ -22,7 +22,7 @@ for mailbox in mailbox_list:
 
     print("Processing Mailbox:", mailbox)
 
-    # vertex_clustering(headers_filename, nodelist_filename, edgelist_filename, foldername)
+    vertex_clustering(headers_filename, nodelist_filename, edgelist_filename, foldername)
     generate_hyperedge_distribution(nodelist_filename, edgelist_filename, headers_filename, foldername)
     generate_keyword_digest(mbox_filename, output_filename=foldername+"/author_keyword_digest.txt", author_uid_filename=author_uid_filename,
                             json_filename=headers_filename, top_n=250, console_output=False)
