5d9cd9c9616e3ef9097cba09996706fad95b7415
aakanksha sanctis <sanctisaakanksha@gmail.com>
2017-11-25 19:35:22 +0530

    Add files via upload
    Main_driver to run all the drivers one after another

---
+++ main_driver.py
@@ -0,0 +1,11 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+
+import driver_headers_mbox
+import driver_author_analysis
+import driver_thread_analysis
+
+driver_headers_mbox
+driver_author_analysis
+driver_thread_analysis
+
