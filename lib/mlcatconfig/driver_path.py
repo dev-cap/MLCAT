3a56bb578197e676ada0f2cbc0004c50c1823380
2015B3A70530G <f20150530@goa.bits-pilani.ac.in>
2018-03-14 21:02:09 +0530

    Config driver class implementation

---
+++ lib/mlcatconfig/driver_path.py
@@ -0,0 +1,20 @@
+import configparser
+import os
+
+# mailbox_list = ['lkml', 'opensuse-kernel', 'opensuse-features', 'opensuse', 'opensuse-bugs', 'opensuse-factory', 'sakai-devel']
+class Config(configparser.ConfigParser):
+    def __init__(self,mailbox):  
+        self.cfgfile = os.path.abspath("mlcat.cfg")
+        super(Config,self).__init__(allow_no_value=True)
+        if os.path.exists(self.cfgfile):
+            self.read(self.cfgfile)
+       	     
+        self.foldername=self.get('param_paths','foldername')+mailbox
+        self.mbox_filename = self.get('param_paths','foldername') + mailbox + '/mbox/' + mailbox + '.mbox'
+        self.clean_headers_filename = self.get('param_paths','foldername') + mailbox + self.get('param_paths','clean_headers_path')
+        self.headers_filename = self.get('param_paths','foldername') + mailbox +self.get('param_paths','headers_path')
+        self.nodelist_filename = self.get('param_paths','foldername') + mailbox  +self.get('param_paths','nodelist_path')
+        self.edgelist_filename = self.get('param_paths','foldername') + mailbox +self.get('param_paths','edgelist_path')
+        self.thread_uid_filename =self.get('param_paths','foldername')+ mailbox +self.get('param_paths','thread_uid_path')
+        self.author_uid_filename = self.get('param_paths','foldername')+ mailbox +self.get('param_paths','author_uid_path') 
+
