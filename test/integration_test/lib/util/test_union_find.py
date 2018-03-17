32e3e99bfe961eb6586dea1e409a08a302d9ae75
tejalwakchoure <35342471+tejalwakchoure@users.noreply.github.com>
2018-03-17 10:13:49 +0530

    Update test_union_find.py

--- test/integration_test/lib/util/test_union_find.py
+++ test/integration_test/lib/util/test_union_find.py
@@ -1,18 +1,27 @@
 from lib.util.union_find import *
 import pytest
 
-def test_union_find(self):
+class TestUnionFind(object):
 
-	ob=UnionFind(5)
-	ob.make_set(5)
-	ob.union(1,2)
-	ob.union(2,5)
-	ob.union(3,4)
+	def test_find(self):	
+		ob=UnionFind(5)
+		ob.make_set(5)
+		ob.union(4,5)
+		assert ob.find(3)==3
+		assert ob.find(5)==4
+		
+	def test_is_connected(self):	
+		ob=UnionFind(5)
+		ob.make_set(5)
+		ob.union(4,2)
+		assert ob.is_connected(3,2)==False
+		assert ob.is_connected(2,4)==True
+		
+	def test_parent(self):	
+		ob=UnionFind(5)
+		ob.make_set(5)
+		ob.union(4,2)
+		ob.union(3,1)		
+		assert ob.parent(2)==4
+		assert ob.parent(1)==3
 	
-	assert ob.find(4)==3
-	assert ob.find(2)==1
-	assert ob.is_connected(3,1)==False
-	assert ob.is_connected(2,5)==True
-	assert ob.parent(5)==1
-	assert ob.parent(4)==3
-
