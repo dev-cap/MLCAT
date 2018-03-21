b5b30006f07d76c2c9499101ddc8141928651083
Hrishikesh Dahiya <f20160057@goa.bits-pilani.ac.in>
2018-03-21 18:01:12 +0530

    Integration test for edge_list.py (#167)
    adds integration tests for edge_list.py module

--- etc/pytest.sh
+++ etc/pytest.sh
@@ -11,6 +11,7 @@ mkdir -p .tmp/integration_test/lib/input/data_cleanup/
 mkdir -p .tmp/integration_test/lib/analysis/author/graph/generate/
 mkdir -p .tmp/integration_test/lib/analysis/author/ranking/
 mkdir -p .tmp/integration_test/lib/analysis/author/wh_table/
+mkdir -p .tmp/integration_test/lib/analysis/author/edge_list/
 
 py.test -v --color=yes --cov=.
 codecov
