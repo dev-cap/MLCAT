633b0cf1888f637867f0ed859793a3af6b0744a4
naveen-hr <jnaveen.hr7@gmail.com>
2018-03-25 22:32:02 +0530

    Integration test for read.py

--- test/integration_test/lib/util/test_read.py
+++ test/integration_test/lib/util/test_read.py
@@ -17,8 +17,28 @@ def test_lines_per_n():
 
 def test_get_utc_time():
 
-	kf1=" Wed, 01 Aug 2007 05:23:20 CET "	
+	kf1=" Wed, 01 Aug 2007 05:23:20 GMT "	
 	utc_dt1=get_utc_time(kf1)
+	assert utc_dt1 == "Wed, 01 Aug 2007 05:23:20 +0000"
+	
+	kf2=" Wed, 01 Aug 2007 04:23:20 PST "	
+	utc_dt2=get_utc_time(kf2)
+	assert utc_dt1 == "Wed, 01 Aug 2007 12:23:20 +0000"
+	
+	kf3=" Wed, 01 Aug 2007 04:23:20 PDT "	
+	utc_dt3=get_utc_time(kf3)
+	assert utc_dt1 == "Wed, 01 Aug 2007 12:23:20 +0000"
+	
+	kf4=" Wed, 01 Aug 2007 04:23:20 EST "	
+	utc_dt4=get_utc_time(kf4)
+	assert utc_dt1 == "Wed, 01 Aug 2007 09:23:20 +0000"
+	
+	kf5=" Wed, 01 Aug 2007 05:23:20 EET "	
+	utc_dt5=get_utc_time(kf5)
+	assert utc_dt1 == "Wed, 01 Aug 2007 03:23:20 +0000"
+	
+	kf6=" Wed, 01 Aug 2007 05:23:20 CET "	
+	utc_dt6=get_utc_time(kf6)
 	assert utc_dt1 == "Wed, 01 Aug 2007 04:23:20 +0000"
 
 def test_get_datetime_object():
@@ -28,6 +48,24 @@ def test_get_datetime_object():
 	mf1=datetime.datetime.strptime("Wed, 01 Aug 2007 04:23:20 +0000", "%a, %d %b %Y %H:%M:%S %z")
 	mf2 = mf1.astimezone(pytz.utc)
 	assert utc_dt1 == mf2
+	
+	mf3=" Wed, 01 Aug 2007 04:23:20 PST "
+	utc_dt1=get_datetime_object(mf3)
+	mf4=datetime.datetime.strptime("Wed, 01 Aug 2007 12:23:20 +0000", "%a, %d %b %Y %H:%M:%S %z")
+	mf5 = mf4.astimezone(pytz.utc)
+	assert utc_dt2 == mf5
+
+	mf6=" Wed, 01 Aug 2007 05:23:20 EST "
+	utc_dt1=get_datetime_object(mf6)
+	mf7=datetime.datetime.strptime("Wed, 01 Aug 2007 10:23:20 +0000", "%a, %d %b %Y %H:%M:%S %z")
+	mf8 = mf7.astimezone(pytz.utc)
+	assert utc_dt3 == mf8
+
+	mf9=" Wed, 01 Aug 2007 05:23:20 EET "
+	utc_dt1=get_datetime_object(mf9)
+	mf10=datetime.datetime.strptime("Wed, 01 Aug 2007 03:23:20 +0000", "%a, %d %b %Y %H:%M:%S %z")
+	mf11 = mf10.astimezone(pytz.utc)
+	assert utc_dt4 == mf11			
 
 def test_get_messages_before():
 	
