e5ca2121e3da3874a3ac5bc7a4e8d0491f66014e
AakankshaSanctis <sanctisaakanksha@gmail.com>
2018-03-27 22:21:12 +0530

    Config driver class and test

--- lib/mlcatconfig/driver_path.py
+++ lib/mlcatconfig/driver_path.py
@@ -3,8 +3,8 @@ import os
 
 # mailbox_list = ['lkml', 'opensuse-kernel', 'opensuse-features', 'opensuse', 'opensuse-bugs', 'opensuse-factory', 'sakai-devel']
 class Config(configparser.ConfigParser):
-    def __init__(self,mailbox):  
-        self.cfgfile = os.path.abspath("mlcat.cfg")
+    def __init__(self,mailbox,cfgfile="mlcat.cfg"):
+        self.cfgfile = os.path.abspath(cfgfile)
         super(Config,self).__init__(allow_no_value=True)
         if os.path.exists(self.cfgfile):
             self.read(self.cfgfile)
@@ -12,13 +12,20 @@ class Config(configparser.ConfigParser):
         self.mailbox=mailbox
         
         
-    def createVariables(self):
-        self.foldername=self.get('param_paths','foldername')+self.mailbox
-        self.mbox_filename = self.get('param_paths','foldername') + self.mailbox + '/mbox/' + self.mailbox + '.mbox'
-        self.clean_headers_filename = self.get('param_paths','foldername') +self.mailbox + self.get('param_paths','clean_headers_path')
-        self.headers_filename = self.get('param_paths','foldername') + self.mailbox +self.get('param_paths','headers_path')
-        self.nodelist_filename = self.get('param_paths','foldername') + self.mailbox  +self.get('param_paths','nodelist_path')
-        self.edgelist_filename = self.get('param_paths','foldername') + self.mailbox +self.get('param_paths','edgelist_path')
-        self.thread_uid_filename =self.get('param_paths','foldername')+ self.mailbox +self.get('param_paths','thread_uid_path')
-        self.author_uid_filename = self.get('param_paths','foldername')+ self.mailbox +self.get('param_paths','author_uid_path') 
+    def createVariables(self,section='param_paths'):
+    	
+        self.foldername=self.get(section,'foldername')+self.mailbox
+        self.mbox_filename = self.get(section,'foldername') + self.mailbox + '/mbox/' + self.mailbox + '.mbox'
+        self.clean_headers_filename = self.get(section,'foldername') +self.mailbox + self.get(section,'clean_headers_path')
+        self.headers_filename = self.get(section,'foldername') + self.mailbox +self.get(section,'headers_path')
+        self.nodelist_filename = self.get(section,'foldername') + self.mailbox  +self.get(section,'nodelist_path')
+        self.edgelist_filename = self.get(section,'foldername') + self.mailbox +self.get(section,'edgelist_path')
+        self.thread_uid_filename =self.get(section,'foldername')+ self.mailbox +self.get(section,'thread_uid_path')
+        self.author_uid_filename = self.get(section,'foldername')+ self.mailbox +self.get(section,'author_uid_path') 
+
+
+#config=Config('openuse')
+#config.createVariables()
+#print(config.foldername)
+
 
--- lib/mlcatconfig/test_Config.py
+++ lib/mlcatconfig/test_Config.py
@@ -1,21 +1,44 @@
 import pytest
 import configparser
 import os
-from lib.mlcatconfig.driver_path import *
+#from testfixtures import TempDirectory
+from driver_path import *
 
+
+
+    
 def test_config_class():
     
     path=os.path.abspath("mlcat.cfg")
+    if ' ' in path:
+        assert False
+        
     t_config=Config('t_mailbox')
     t_config.read(path)
     t_config.createVariables();
     
     assert t_config.foldername =='./data/t_mailbox'
     assert t_config.mbox_filename=='./data/t_mailbox/mbox/t_mailbox.mbox'
-    assert t_config.clean_headers_path=='./data/t_mailbox/json/clean_data.json'
-    assert t_config.nodelist_path =='./data/t_mailbox/tables/graph_nodes.csv'
-    assert t_config.edgelist_path =='./data/t_mailbox/tables/graph_edges.csv'
-    assert t_config.thread_uid_path =='.data/t_mailbox/json/thread_uid_map.json'
-    assert t_config.author_uid_path =='./data/t_mailbox/json/author_uid_map.json'
-    assert t_config.headers_path =='./data/t_mailbox/json/headers.json'
-
+    assert t_config.clean_headers_filename=='./data/t_mailbox/json/clean_data.json'
+    assert t_config.nodelist_filename =='./data/t_mailbox/tables/graph_nodes.csv'
+    assert t_config.edgelist_filename =='./data/t_mailbox/tables/graph_edges.csv'
+    assert t_config.thread_uid_filename =='./data/t_mailbox/json/thread_uid_map.json'
+    assert t_config.author_uid_filename =='./data/t_mailbox/json/author_uid_map.json'
+    assert t_config.headers_filename =='./data/t_mailbox/json/headers.json'
+    
+    
+@pytest.fixture(scope='session')
+def test_diff_dir_config(tmpdir_factory):
+    
+    p = tmpdir_factory.mkdir("test_dir").join("test_file.cfg")
+    cfgfile = open(p, 'w')
+    test_cfg = ConfigParser.ConfigParser()
+    test_cfg.add_section('path_test')
+    test_cfg.set('path_test', 'foldername', './t_data/test_file/')
+    test_cfg.write(cfgfile)
+    cfgfile.close()
+    
+    t_config=Config('t_mailbox',p)
+    t_config.read(p.path)
+    t_config.createVariables('path_test');
+    assert t_config.foldername=='./t_data/test_file/t_mailbox'
