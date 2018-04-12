1caaf6e38de1e5e7bdbebdaffb4075b9e1c4f33b
tejalwakchoure <35342471+tejalwakchoure@users.noreply.github.com>
2018-04-12 17:25:37 +0530

    Update driver_headers_imap.py

--- lib/deprecated/driver_headers_imap.py
+++ lib/deprecated/driver_headers_imap.py
@@ -5,11 +5,12 @@ from lib.input.data_cleanup import *
 # Uncomment the following line to update "uid_map.json"
 # write_uid_map(1, 57635)
 
-last_uid = check_validity(True)
-remove_duplicate_headers()
-remove_unwanted_headers()
-add_missing_headers()
-replace_invalid_headers()
+obj=CheckHeaders()
+last_uid = obj.check_validity(True)
+obj.remove_duplicate_headers()
+obj.remove_unwanted_headers()
+obj.add_missing_headers()
+obj.replace_invalid_headers()
 
 get_mail_header([last_uid+1], True)
 remove_invalid_references()
