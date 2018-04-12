9f95782b167e3e2d17bc6efb6716cbfdf0db2c43
tejalwakchoure <35342471+tejalwakchoure@users.noreply.github.com>
2018-04-12 17:18:56 +0530

    Update test_check_headers.py

--- test/integration_test/lib/input/test_check_headers.py
+++ test/integration_test/lib/input/test_check_headers.py
@@ -1,68 +1,88 @@
-import unittest 
-import mock
-from lib.input.check_headers import *
+#import unittest 
+from unittest import mock
+import json
+from lib.util.read import lines_per_n
+from lib.input.check_headers import CheckHeaders
 
-headers_file='./test/integration_test/data/headers_for_check.json'
-unwanted_uid_file='./test/integration_test/data/unwanted_uid.txt'
-uid_map_file='./test/integration_test/data/thread_uid_map.json'
 
-@mock.patch('lib.input.check_headers.open_connection')
-def test_get_unavailable_uid(mock_function):
-
-	mock_function.return_value.uid.return_value=(1,['5'])
-	assert get_unavailable_uid()==set()
+class TestCheckHeaders(object):	
 	
-@mock.patch('lib.input.check_headers.open_connection')
-def test_check_validity(mock_function):
+	headers_file='/home/tejal/MLCAT/test/integration_test/data/headers_for_check.json'
+	unwanted_uid_file='/home/tejal/MLCAT/test/integration_test/data/unwanted_uid.txt'
+	uid_map_file='/home/tejal/MLCAT/test/integration_test/data/thread_uid_map.json'
 
-	mock_function.return_value.uid.return_value=(1,['5'])
-	assert check_validity(True, headers_file)==5
+
+	@mock.patch('lib.input.check_headers.open_connection')
+	def test_get_unavailable_uid(self,mock_function):
+		
+		obj=CheckHeaders()
+		mock_function.return_value.uid.return_value=(1,['5'])
+		assert obj.get_unavailable_uid()==set()
 	
-def test_remove_unwanted_headers():
 
-	check_validity(False,headers_file)
-	remove_unwanted_headers(unwanted_uid,headers_file)
-	with open(headers_file, 'r') as json_file:
-		for chunk in lines_per_n(json_file, 9):
-			json_obj = json.loads(chunk)
-			assert json_obj['Message-ID'] !=3
+	@mock.patch('lib.input.check_headers.open_connection')
+	def test_check_validity(self,mock_function):
+
+		mock_function.return_value.uid.return_value=(1,['5'])
+		obj=CheckHeaders()
+		assert obj.check_validity(True, self.headers_file)==5
 	
-def test_remove_duplicate_headers():
 
-	check_validity(False, headers_file)
-	remove_duplicate_headers(duplicate_uid,headers_file)	
-	count_uid=0
-	with open(headers_file, 'r') as json_file:
-		for chunk in lines_per_n(json_file, 9):
-			json_obj = json.loads(chunk)
-			if json_obj['Message-ID'] ==2:
-				count_uid=count_uid+1
-	assert count_uid==1
+	def test_remove_unwanted_headers(self):
 
-@mock.patch('lib.input.imap.header.open_connection')
-def test_add_missing_headers(mock_function):
+		obj=CheckHeaders()
+		obj.check_validity(False,self.headers_file)
+		obj.remove_unwanted_headers(obj.unwanted_uid,self.headers_file)
+		with open(self.headers_file, 'r') as json_file:
+			for chunk in lines_per_n(json_file, 9):
+				json_obj = json.loads(chunk)
+				assert json_obj['Message-ID'] !=3
+	
 
-	mock_function.return_value.uid.return_value=(1,['5'])
-	with open(headers_file, 'r') as json_file:
-		for chunk in lines_per_n(json_file, 9):
-			json_obj = json.loads(chunk)
-	check_validity(False, headers_file)
-	add_missing_headers(missing_uid,unwanted_uid_file,uid_map_file)	
-	if(missing_uid):
+	def test_remove_duplicate_headers(self):
+
+		obj=CheckHeaders()
+		obj.check_validity(False, self.headers_file)
+		obj.remove_duplicate_headers(obj.duplicate_uid, self.headers_file)	
+		count_uid=0
+		with open(self.headers_file, 'r') as json_file:
+			for chunk in lines_per_n(json_file, 9):
+				json_obj = json.loads(chunk)
+				if json_obj['Message-ID'] ==2:
+					count_uid=count_uid+1
+		assert count_uid==1
+
+
+	@mock.patch('lib.input.imap.header.open_connection')
+	def test_add_missing_headers(self,mock_function):
+
+		mock_function.return_value.uid.return_value=(1,['5'])
+		obj=CheckHeaders()		
+		with open(self.headers_file, 'r') as json_file:
+			for chunk in lines_per_n(json_file, 9):
+				json_obj = json.loads(chunk)		
+		obj.check_validity(False, self.headers_file)
+		obj.add_missing_headers(obj.missing_uid,self.unwanted_uid_file,self.uid_map_file)	
+		if(obj.missing_uid):
+			mock_function.assert_any_call()
+
+	
+	@mock.patch('lib.input.imap.header.open_connection')
+	def test_replace_invalid_headers(self,mock_function):
+		
+		mock_function.return_value.uid.return_value=(1,['5'])
+		obj=CheckHeaders()
+		obj.check_validity(False, self.headers_file)
+		obj.replace_invalid_headers(obj.invalid_uid,self.headers_file,self.unwanted_uid_file,self.uid_map_file)
+		if(obj.invalid_uid):
+			mock_function.assert_any_call()
+
+	
+	@mock.patch('lib.input.check_headers.open_connection')
+	def test_write_uid_map(self,mock_function):
+		
+		mock_function.return_value.uid.return_value=(1,['5'])
+		obj=CheckHeaders()
+		obj.check_validity(False, self.headers_file)
+		obj.write_uid_map(1, obj.last_uid_read, self.uid_map_file)
 		mock_function.assert_any_call()
-
-@mock.patch('lib.input.imap.header.open_connection')
-def test_replace_invalid_headers(mock_function):
-	mock_function.return_value.uid.return_value=(1,['5'])
-	check_validity(False, headers_file)
-	replace_invalid_headers(invalid_uid,headers_file,unwanted_uid_file,uid_map_file)
-	if(invalid_uid):
-		mock_function.assert_any_call()
-
-@mock.patch('lib.input.check_headers.open_connection')
-def test_write_uid_map(mock_function):
-	mock_function.return_value.uid.return_value=(1,['5'])
-	check_validity(False, headers_file)
-	write_uid_map(1,last_uid_read,uid_map_file)
-	mock_function.assert_any_call()
-	
