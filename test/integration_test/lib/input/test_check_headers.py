0dd945afb9673815754043c92a3ff38501a796d1
tejalwakchoure <35342471+tejalwakchoure@users.noreply.github.com>
2018-04-13 10:46:24 +0530

    Update test_check_headers.py

--- test/integration_test/lib/input/test_check_headers.py
+++ test/integration_test/lib/input/test_check_headers.py
@@ -15,24 +15,24 @@ class TestCheckHeaders(object):
 	@mock.patch('lib.input.check_headers.open_connection')
 	def test_get_unavailable_uid(self,mock_function):
 		
-		obj=CheckHeaders()
+		checkHeaders=CheckHeaders()
 		mock_function.return_value.uid.return_value=(1,['5'])
-		assert obj.get_unavailable_uid()==set()
+		assert checkHeaders.get_unavailable_uid()==set()
 	
 
 	@mock.patch('lib.input.check_headers.open_connection')
 	def test_check_validity(self,mock_function):
 
 		mock_function.return_value.uid.return_value=(1,['5'])
-		obj=CheckHeaders()
-		assert obj.check_validity(True, self.headers_file)==5
+		checkHeaders=CheckHeaders()
+		assert checkHeaders.check_validity(True, self.headers_file)==5
 	
 
 	def test_remove_unwanted_headers(self):
 
-		obj=CheckHeaders()
-		obj.check_validity(False,self.headers_file)
-		obj.remove_unwanted_headers(obj.unwanted_uid,self.headers_file)
+		checkHeaders=CheckHeaders()
+		checkHeaders.check_validity(False,self.headers_file)
+		checkHeaders.remove_unwanted_headers(checkHeaders.unwanted_uid,self.headers_file)
 		with open(self.headers_file, 'r') as json_file:
 			for chunk in lines_per_n(json_file, 9):
 				json_obj = json.loads(chunk)
@@ -41,9 +41,9 @@ class TestCheckHeaders(object):
 
 	def test_remove_duplicate_headers(self):
 
-		obj=CheckHeaders()
-		obj.check_validity(False, self.headers_file)
-		obj.remove_duplicate_headers(obj.duplicate_uid, self.headers_file)	
+		checkHeaders=CheckHeaders()
+		checkHeaders.check_validity(False, self.headers_file)
+		checkHeaders.remove_duplicate_headers(checkHeaders.duplicate_uid, self.headers_file)	
 		count_uid=0
 		with open(self.headers_file, 'r') as json_file:
 			for chunk in lines_per_n(json_file, 9):
@@ -57,13 +57,13 @@ class TestCheckHeaders(object):
 	def test_add_missing_headers(self,mock_function):
 
 		mock_function.return_value.uid.return_value=(1,['5'])
-		obj=CheckHeaders()		
+		checkHeaders=CheckHeaders()		
 		with open(self.headers_file, 'r') as json_file:
 			for chunk in lines_per_n(json_file, 9):
 				json_obj = json.loads(chunk)		
-		obj.check_validity(False, self.headers_file)
-		obj.add_missing_headers(obj.missing_uid,self.unwanted_uid_file,self.uid_map_file)	
-		if(obj.missing_uid):
+		checkHeaders.check_validity(False, self.headers_file)
+		checkHeaders.add_missing_headers(checkHeaders.missing_uid,self.unwanted_uid_file,self.uid_map_file)	
+		if(checkHeaders.missing_uid):
 			mock_function.assert_any_call()
 
 	
@@ -71,10 +71,10 @@ class TestCheckHeaders(object):
 	def test_replace_invalid_headers(self,mock_function):
 		
 		mock_function.return_value.uid.return_value=(1,['5'])
-		obj=CheckHeaders()
-		obj.check_validity(False, self.headers_file)
-		obj.replace_invalid_headers(obj.invalid_uid,self.headers_file,self.unwanted_uid_file,self.uid_map_file)
-		if(obj.invalid_uid):
+		checkHeaders=CheckHeaders()
+		checkHeaders.check_validity(False, self.headers_file)
+		checkHeaders.replace_invalid_headers(checkHeaders.invalid_uid,self.headers_file,self.unwanted_uid_file,self.uid_map_file)
+		if(checkHeaders.invalid_uid):
 			mock_function.assert_any_call()
 
 	
@@ -82,7 +82,7 @@ class TestCheckHeaders(object):
 	def test_write_uid_map(self,mock_function):
 		
 		mock_function.return_value.uid.return_value=(1,['5'])
-		obj=CheckHeaders()
-		obj.check_validity(False, self.headers_file)
-		obj.write_uid_map(1, obj.last_uid_read, self.uid_map_file)
+		checkHeaders=CheckHeaders()
+		checkHeaders.check_validity(False, self.headers_file)
+		checkHeaders.write_uid_map(1, checkHeaders.last_uid_read, self.uid_map_file)
 		mock_function.assert_any_call()
