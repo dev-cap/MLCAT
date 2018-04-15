3ff947c938fa43518c3c6aa0952050bcdb807790
hrishikesh-dahiya <f20160057@goa.bits-pilani.ac.in>
2018-04-15 15:58:55 +0530

    Update test_read.py

--- test/integration_test/lib/util/test_read.py
+++ test/integration_test/lib/util/test_read.py
@@ -17,17 +17,48 @@ def test_lines_per_n():
 
 def test_get_utc_time():
 
-	kf1=" Wed, 01 Aug 2007 05:23:20 CET "	
-	utc_dt1=get_utc_time(kf1)
-	assert utc_dt1 == "Wed, 01 Aug 2007 04:23:20 +0000"
+	date1 = " Wed, 01 Aug 2007 05:23:20 GMT "	
+	date2 = " Wed, 01 Aug 2007 05:23:20 PST "
+	date3 = " Wed, 01 Aug 2007 05:23:20 PDT "
+	date4 = " Wed, 01 Aug 2007 05:23:20 EST "
+	date5 = " Wed, 01 Aug 2007 05:23:20 EET "
+	date6 = " Wed, 01 Aug 2007 05:23:20 CET "
+
+	utc_dt1 = get_utc_time(date1)
+	utc_dt2 = get_utc_time(date2)
+	utc_dt3 = get_utc_time(date3)
+	utc_dt4 = get_utc_time(date4)
+	utc_dt5 = get_utc_time(date5)
+	utc_dt6 = get_utc_time(date6)
+	
+	assert utc_dt1 == "Wed, 01 Aug 2007 05:23:20 +0000"
+	assert utc_dt2 == "Wed, 01 Aug 2007 13:23:20 +0000"
+	assert utc_dt3 == "Wed, 01 Aug 2007 13:23:20 +0000"
+	assert utc_dt4 == "Wed, 01 Aug 2007 10:23:20 +0000"
+	assert utc_dt5 == "Wed, 01 Aug 2007 03:23:20 +0000"
+	assert utc_dt6 == "Wed, 01 Aug 2007 04:23:20 +0000"
+
 
 def test_get_datetime_object():
 
-	mf=" Wed, 01 Aug 2007 05:23:20 CET "
-	utc_dt1=get_datetime_object(mf)
-	mf1=datetime.datetime.strptime("Wed, 01 Aug 2007 04:23:20 +0000", "%a, %d %b %Y %H:%M:%S %z")
-	mf2 = mf1.astimezone(pytz.utc)
-	assert utc_dt1 == mf2
+	date1 = "  01 Aug 2007 05:23:20 GMT "	
+	date2 = " Wed, 01 Aug 2007 05:23:20 PST "
+	date3 = " Wed, 01 Aug 2007 05:23:20 EST "
+	date4 = " Wed, 01 Aug 2007 05:23:20 EET "
+	date5 = " Wed, 01 Aug 2007 05:23:20 CET "
+
+	utc_dt1 = get_datetime_object(date1)
+	utc_dt2 = get_datetime_object(date2)
+	utc_dt3 = get_datetime_object(date3)
+	utc_dt4 = get_datetime_object(date4)
+	utc_dt5 = get_datetime_object(date5)
+	
+	assert str(utc_dt1) == "2007-08-01 05:23:20+00:00"
+	assert str(utc_dt2) == "2007-08-01 13:23:20+00:00"
+	assert str(utc_dt3) == "2007-08-01 10:23:20+00:00"
+	assert str(utc_dt4) == "2007-08-01 03:23:20+00:00"
+	assert str(utc_dt5) == "2007-08-01 04:23:20+00:00"
+
 
 def test_get_messages_before():
 	
@@ -36,6 +67,7 @@ def test_get_messages_before():
 	n=get_messages_before(nf,file_name)
 	n1={43639,11622,37690,18081,50823,24533,176,38086,40158,51348}
 	assert n == n1
+
 
 def test_get_lone_author_threads():
     
