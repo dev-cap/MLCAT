389e34385db63740b866e82605a8b357eec63d4f
hrishikesh-dahiya <f20160057@goa.bits-pilani.ac.in>
2018-04-18 22:41:04 +0530

    update test_curve_fitting.py

--- test/integration_test/lib/analysis/author/test_curve_fiting.py
+++ test/integration_test/lib/analysis/author/test_curve_fiting.py
@@ -16,7 +16,7 @@ def test_generate_crt_dist():
     csv_filename = './test/integration_test/data/conversation_refresh_times.csv'
     req_re_times = ([106.01, 272.03000000000003, 438.05000000000007, 604.07, 770.09, 936.1100000000001, 1102.13, 1268.15, 1434.17, 1600.19, 1766.21, 1932.23, 2098.25, 2264.2700000000004, 2430.29, 2596.3100000000004, 2762.33, 2928.3500000000004, 3094.37, 3260.3900000000003, 3426.41, 3592.4300000000003, 3758.45, 3924.4700000000003, 4090.4900000000002, 4256.51, 4422.530000000001, 4588.55, 4754.57, 4920.59, 5086.610000000001, 5252.63, 5418.650000000001, 5584.67, 5750.6900000000005, 5916.710000000001, 6082.7300000000005, 6248.75, 6414.77, 6580.790000000001, 6746.81, 6912.83, 7078.85, 7244.870000000001, 7410.89, 7576.91, 7742.93, 7908.950000000001, 8074.970000000001, 8240.99], [0.375, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125])
     generate_crt_dist(csv_filename) == req_re_times
-    
+
 
 @mock.patch("matplotlib.pyplot.figure")
 @mock.patch("matplotlib.pyplot.plot")
@@ -26,7 +26,7 @@ def test_generate_crt_dist():
 @mock.patch("matplotlib.pyplot.savefig")
 def test_generate_crt_curve_fits(mock_figure, mock_plot, mock_legend, mock_ylabel, mock_xlabel, mock_savefig):
     
-    foldername = './test/integration_test/data/crt_curve_fits/'
+    foldername = './test/integration_test/data/curve_fitting/'
 
     generate_crt_curve_fits(foldername)
 
@@ -70,3 +70,26 @@ def test_generate_rt_dist():
     csv_filename = './test/integration_test/data/response_time.csv'
     req_times = ([783.76, 1441.28, 2098.8, 2756.3199999999997, 3413.84, 4071.3599999999997, 4728.879999999999, 5386.4, 6043.92, 6701.4400000000005, 7358.959999999999, 8016.48, 8674.0, 9331.52, 9989.039999999999, 10646.56, 11304.08, 11961.6, 12619.119999999999, 13276.64, 13934.16, 14591.68, 15249.199999999999, 15906.72, 16564.239999999998, 17221.760000000002, 17879.28, 18536.8, 19194.32, 19851.839999999997, 20509.36, 21166.879999999997, 21824.4, 22481.92, 23139.440000000002, 23796.96, 24454.48, 25112.0, 25769.519999999997, 26427.04, 27084.559999999998, 27742.08, 28399.6, 29057.12, 29714.64, 30372.159999999996, 31029.68, 31687.199999999997, 32344.72, 33002.24], [0.1111111111111111, 0.2222222222222222, 0.2222222222222222, 0.0, 0.1111111111111111, 0.2222222222222222, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1111111111111111])
     assert generate_rt_dist(csv_filename) == req_times
+
+
+@mock.patch("matplotlib.pyplot.figure")
+@mock.patch("matplotlib.pyplot.plot")
+@mock.patch("matplotlib.pyplot.legend")
+@mock.patch("matplotlib.pyplot.ylabel")
+@mock.patch("matplotlib.pyplot.xlabel")
+@mock.patch("matplotlib.pyplot.savefig")
+def test_generate_rt_curve_fits(mock_figure, mock_plot, mock_legend, mock_ylabel, mock_xlabel, mock_savefig):
+    
+    foldername = './test/integration_test/data/curve_fitting/'
+
+    generate_rt_curve_fits(foldername)
+
+    assert mock_figure.assert_called()
+    assert mock_plot.assert_called()
+    assert mock_legend.assert_called()
+    assert mock_ylabel.assert_called()
+    assert mock_xlabel.assert_called()
+    assert mock_savefig.assert_called()
+
+
+
